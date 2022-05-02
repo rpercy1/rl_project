77ef85c39ce89e6a02209dc335105be4ef7f5a98
Caleb Roberson <caleb.roberson@gmail.com>
2022-05-02 17:42:45 -0400

    adding file loading error checking

--- project_code.py
+++ project_code.py
@@ -1,11 +1,14 @@
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 
-all_random = pd.read_csv(r"C:/Users/percy/OneDrive - University of Tennessee/MSBA/BZAN 583 Reinforcement/random/all.csv")
-#caleb's path
-all_random = pd.read_csv(r"C:/Users/caleb/Downloads/GroupAssignmentRecommender.zip/data/all_random/all.csv")
+#read in the data
+try:
+    all_random = pd.read_csv(r"C:/Users/caleb/Downloads/GroupAssignmentRecommender/data/all_random/all.csv")
+except FileNotFoundError:
+    all_random = pd.read_csv(r"C:/Users/percy/OneDrive - University of Tennessee/MSBA/BZAN 583 Reinforcement/random/all.csv")
 
-all_random.head()
+all_random.tail()
 
 ################# Generate Unique Customer Groups ##########################
 all_random.info() # 4 user features, use this to group users
@@ -13,6 +16,8 @@ len(all_random.user_feature_0.unique()) #4 options
 len(all_random.user_feature_1.unique()) #6 options
 len(all_random.user_feature_2.unique()) #10 options
 len(all_random.user_feature_3.unique()) #10 options
+
+all_random.position.unique()
 
 possible_combos = 4*6*10*10 #2400
 
